823fa886d39caceccb315d819548c022561e5263
Jordi Tielens <jordi.tielens@student.kuleuven.com>
2011-10-24 10:18:20 +0200

    test

--- src/be/kuleuven/mume/PMF.java
+++ src/be/kuleuven/mume/PMF.java
@@ -4,6 +4,7 @@ import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManagerFactory;
 
 public final class PMF {
+//Test
 	private static final PersistenceManagerFactory pmfInstance = JDOHelper
 			.getPersistenceManagerFactory("transactions-optional");
 
